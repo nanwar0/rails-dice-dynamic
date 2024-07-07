1406db5ac4c2d7d351eb0f0273ce1f312cb23f84
nanwar0 <nina.anwar@chicagobooth.edu>
2024-07-07 19:09:34 +0000

    final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
   def home
     render({:template => "pages_templates/home"})
   end
+
+  def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @num_sides = params.fetch("number_of_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+
+      @rolls.push(die)
+    end
+    render({:template => "pages_templates/roll"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", {:controller => "pages", :action => "home"})
+  get("/dice/:number_of_dice/:number_of_sides", {:controller => "pages", :action => "roll"})
 end
